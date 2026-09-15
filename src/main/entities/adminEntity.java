8c47a460f5db3fe5d67e537116f946d2d7820769
AryanLeap <electrowiz67@gmail.com>
2026-09-15 14:13:38 +0000

    Added a best practice addition forgot before adding a column to the admin id for explicity mapping

--- src/main/entities/adminEntity.java
+++ src/main/entities/adminEntity.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 public class adminEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
     private Integer adminId;
     @JsonProperty("username")
     @Column(name = "username")

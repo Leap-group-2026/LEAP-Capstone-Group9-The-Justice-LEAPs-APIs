4169cdf3fa8a8f5b71c3fe45d17e43894c50eb55
MichaelH3rnandez <mikiejh@icloud.cojm>
2026-09-16 21:34:40 +0000

    Adding the entity and repo for transactions

---
+++ src/main/entities/transactionsEntity.java
@@ -0,0 +1,79 @@
+package main.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.cglib.core.Local;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Entity 
+@Table(name = "transactions")
+public class transactionsEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer transactionId;
+    @JsonProperty("amount")
+    @Column(name = "amount")
+    private BigDecimal amount;
+    @JsonProperty("side")
+    @Column(name = "side")
+    private String side;
+    @Column(name = "account_id")
+    private Integer accountId;
+    @JsonProperty("transaction_type")
+    @Column(name = "transaction_type")
+    private String transactionType;
+    @JsonProperty("happened_at")
+    @Column(name = "happened_at")
+    private LocalDateTime happenedAt;
+
+    //Getters and Setters
+    public Integer getTransactionId() {
+        return transactionId;
+    }
+    public void setTransactionId(Integer transactionId) {
+        this.transactionId = transactionId;
+    }
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+    public String getSide() {
+        return side;
+    }
+    public void setSide(String side) {
+        this.side = side;
+    }
+    public Integer getAccountId() {
+        return accountId;
+    }
+    public void setAccountId(Integer accountId) {
+        this.accountId = accountId;
+    }
+    public String getTransactionType() {
+        return transactionType;
+    }
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+    public LocalDateTime getHappenedAt() {
+        return happenedAt;
+    }
+    public void setHappenedAt(LocalDateTime happenedAt) {
+        this.happenedAt = happenedAt;
+    }
+}
+

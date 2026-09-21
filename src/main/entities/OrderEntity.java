cd0587d8ac9f72afbd7ca21059045174d2f1a4d9
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-09-21 23:36:47 +0000

    refactor: removed the validation imports and annotations from the order entity, will be moving them to DTOs to validate request bodies not on repo operations

--- src/main/entities/OrderEntity.java
+++ src/main/entities/OrderEntity.java
@@ -1,8 +1,6 @@
 package main.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import org.hibernate.annotations.CreationTimestamp;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.NoArgsConstructor;
@@ -22,42 +20,34 @@ public class OrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer orderId;
-    
-    @NotNull
+
     @Setter
     @JsonProperty("side")
     @Column(name = "side", nullable = false)
     private String side;
-    
-    @NotNull
+
     @Setter
     @JsonProperty("account_id")
     @ManyToOne
     @JoinColumn(name = "account_id", nullable = false)
     private accountsEntity accountId;
-    
-    @NotNull
+
     @Setter
     @JsonProperty("instrument_id")
     @ManyToOne
     @JoinColumn(name = "instrument_id", nullable = false)
     private instrumentEntity instrumentId;
 
-    @NotNull
     @Setter
     @JsonProperty("status")
     @Column(name = "status", nullable = false)
     private String status;
-    
-    @NotNull
-    @Positive
+
     @Setter
     @JsonProperty("quantity")
     @Column(name = "quantity", nullable = false)
     private Integer quantity;
-    
-    @NotNull
-    @Positive
+
     @Setter
     @JsonProperty("total_price")
     @Column(name = "total_price", nullable = false, columnDefinition = "NUMERIC")
@@ -70,8 +60,7 @@ public class OrderEntity {
     @CreationTimestamp
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
-    
-    // Custom constructor for order creation (5 params)
+
     public OrderEntity(String side, accountsEntity accountId, instrumentEntity instrumentId,
                        Integer quantity, BigDecimal totalPrice) {
         this.side = side;

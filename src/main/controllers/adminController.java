3671e7b591e231cea81450a7ad1a9f27413798c4
AryanLeap <electrowiz67@gmail.com>
2026-09-21 20:20:23 +0000

    Added both admin routes for logging in and creating an account, added tests for it, and a basic global exception handler class

--- src/main/controllers/adminController.java
+++ src/main/controllers/adminController.java
@@ -2,17 +2,28 @@ package main.controllers;
 
 import main.services.AdminService;
 import main.entities.adminEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RestController
 @RequestMapping("/admin")
 public class adminController {
     private AdminService service;
-    public adminController(AdminService service){
+    private PasswordEncoder passwordEncoder;
+    public adminController(AdminService service, PasswordEncoder passwordEncoder){
         this.service = service;
+        this.passwordEncoder = passwordEncoder;
     }
     @PostMapping("/create")
     public adminEntity createAdmin(@RequestBody adminEntity admin){
+        String pass = passwordEncoder.encode(admin.getPassHash());
+        admin.setPassHash(pass);
         return service.saveAdmin(admin);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody adminEntity admin){
+        return service.login(admin);
     }
 }

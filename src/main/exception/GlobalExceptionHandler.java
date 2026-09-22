0cbf905a22e7ca92948312a37afe1859c1bb6d1e
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-09-22 00:02:48 +0000

    feat: created a global exception handler that will catch errors thrown by any of the validation annotations attached to a request DTO and returns error response DTOs

---
+++ src/main/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package main.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.validation.FieldError;
+
+import main.dto.response.ValidationError;
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        FieldError fieldError = ex.getBindingResult().getFieldError();
+        
+        ValidationError error = new ValidationError(
+            HttpStatus.BAD_REQUEST.value(),
+            "Validation failed",
+            LocalDateTime.now(),
+            fieldError.getField(),
+            fieldError.getRejectedValue(),
+            fieldError.getDefaultMessage()
+        );
+
+        return ResponseEntity.badRequest().body(error);
+    }
+}

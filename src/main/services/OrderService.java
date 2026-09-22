3c1fa5bd701004a7638cdf8c64952b95596d2ab0
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-09-22 00:06:40 +0000

    feat: created a createOrder method in the OrderService class which saves a test order entity to the database

--- src/main/services/OrderService.java
+++ src/main/services/OrderService.java
@@ -2,13 +2,45 @@ package main.services;
 
 import org.springframework.stereotype.Service;
 
+import main.dto.request.CreateOrderRequest;
+import main.entities.OrderEntity;
+import main.entities.accountsEntity;
+import main.entities.instrumentEntity;
 import main.repos.OrdersRepo;
+import main.repos.AccountsRepo;
+import main.repos.instrumentRepo;
+
+// Imports to delete later
+import java.math.BigDecimal;
 
 @Service
 public class OrderService {
     private final OrdersRepo ordersRepo;
+    private final AccountsRepo accountsRepo;
+    private final instrumentRepo instrumentRepo;
 
-    public OrderService(OrdersRepo repo){
-        this.ordersRepo = repo;
+    public OrderService(OrdersRepo ordersRepo, AccountsRepo accountsRepo, instrumentRepo instrumentRepo) {
+        this.ordersRepo = ordersRepo;
+        this.accountsRepo = accountsRepo;
+        this.instrumentRepo = instrumentRepo;
+    }
+
+    public OrderEntity createOrder(CreateOrderRequest request) {
+        // Will separate this out to different methods and create custom exceptions
+        accountsEntity account = accountsRepo.findById(request.accountId())
+            .orElseThrow(() -> new IllegalArgumentException("Account not found"));
+
+        instrumentEntity instrument = instrumentRepo.findById(request.instrumentId())
+            .orElseThrow(() -> new IllegalArgumentException("Instrument not found"));
+
+        OrderEntity order = new OrderEntity(
+            request.side(),
+            account,
+            instrument,
+            request.quantity(),
+            BigDecimal.valueOf(420.69)
+        );
+
+        return ordersRepo.save(order);
     }
 }

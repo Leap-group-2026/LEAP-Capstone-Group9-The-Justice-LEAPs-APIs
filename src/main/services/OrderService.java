e2fffe9793a2bac8b733c591c68ba27b5fa5c51c
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-09-23 14:00:53 +0000

    refactor: refactored OrderService to use new resolver, validator, and calculation dependencies

--- src/main/services/OrderService.java
+++ src/main/services/OrderService.java
@@ -6,41 +6,54 @@ import main.dto.request.CreateOrderRequest;
 import main.entities.OrderEntity;
 import main.entities.accountsEntity;
 import main.entities.instrumentEntity;
-import main.exception.ResourceNotFoundException;
 import main.repos.OrdersRepo;
-import main.repos.AccountsRepo;
-import main.repos.instrumentRepo;
-
-// TODO: delete import after implementing order total calculation
+import main.services.validation.BuyOrderValidator;
+import main.services.calculation.OrderPriceCalculator;
+import main.services.resolver.AccountResolver;
+import main.services.resolver.InstrumentResolver;
 import java.math.BigDecimal;
 
 @Service
 public class OrderService {
     private final OrdersRepo ordersRepo;
-    private final AccountsRepo accountsRepo;
-    private final instrumentRepo instrumentRepo;
+    private final BuyOrderValidator buyOrderValidator;
+    private final OrderPriceCalculator priceCalculator;
+    private final AccountResolver accountResolver;
+    private final InstrumentResolver instrumentResolver;
 
-    public OrderService(OrdersRepo ordersRepo, AccountsRepo accountsRepo, instrumentRepo instrumentRepo) {
+    public OrderService(OrdersRepo ordersRepo, BuyOrderValidator buyOrderValidator, 
+                       OrderPriceCalculator priceCalculator, AccountResolver accountResolver,
+                       InstrumentResolver instrumentResolver) {
         this.ordersRepo = ordersRepo;
-        this.accountsRepo = accountsRepo;
-        this.instrumentRepo = instrumentRepo;
+        this.buyOrderValidator = buyOrderValidator;
+        this.priceCalculator = priceCalculator;
+        this.accountResolver = accountResolver;
+        this.instrumentResolver = instrumentResolver;
     }
 
     public OrderEntity createOrder(CreateOrderRequest request) {
-        // TODO: separate this out to different methods and create custom exceptions
-        accountsEntity account = accountsRepo.findById(request.accountId())
-            .orElseThrow(() -> new ResourceNotFoundException("Account", request.accountId().toString()));
+        accountsEntity account = accountResolver.resolve(request.accountId());
+        instrumentEntity instrument = instrumentResolver.resolve(request.instrumentId());
 
-        instrumentEntity instrument = instrumentRepo.findById(request.instrumentId())
-            .orElseThrow(() -> new ResourceNotFoundException("Instrument", request.instrumentId().toString()));
-
-        // TODO: add validation later on before creating entities
+        String orderSide = request.side();
+        BigDecimal totalPrice = BigDecimal.ZERO;
+        
+        if ("BUY".equals(orderSide)) {
+            buyOrderValidator.validate(request, account, instrument);
+            totalPrice = priceCalculator.calculateOrderPrice(instrument, request.quantity());
+        }
+        else if ("SELL".equals(orderSide)) {
+            // TODO: SellOrderValidator will be used here when available
+        } else {
+            // TODO: return an error response, invalid side
+        }
+        
         OrderEntity order = new OrderEntity(
-            request.side(),
+            orderSide,
             account,
             instrument,
             request.quantity(),
-            BigDecimal.valueOf(420.69)
+            totalPrice
         );
 
         return ordersRepo.save(order);

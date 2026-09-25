2ffc4bc8d2c3ec5301db1cef1ed733c26ee41862
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-09-25 16:03:11 +0000

    refactor: changed calculateOrderPrice method to be static so that the calculator no longer needs to be added as a dependency in places like the OrderService

--- src/main/services/calculation/OrderPriceCalculator.java
+++ src/main/services/calculation/OrderPriceCalculator.java
@@ -1,13 +1,11 @@
 package main.services.calculation;
 
-import org.springframework.stereotype.Component;
 import main.entities.InstrumentEntity;
 import java.math.BigDecimal;
 
-@Component
 public class OrderPriceCalculator {
-    
-    public BigDecimal calculateOrderPrice(InstrumentEntity instrument, Integer quantity) {
+
+    public static BigDecimal calculateOrderPrice(InstrumentEntity instrument, Integer quantity) {
         BigDecimal instrumentPrice = instrument.getPrice();
 
         return instrumentPrice.multiply(BigDecimal.valueOf(quantity));
